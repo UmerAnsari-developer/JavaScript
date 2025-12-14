b858cbb0d16f989dddc9d4b898f88b860062e2fa
Mohd. Umer Ansari <ua7088807748@gmail.com>
2025-12-14 17:59:59 +0000

    Variables concept done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144552
+let accountEmail = "umer@gamil.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accointId =2 //Not allowed
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail = "ua@gamil.com"
+accountPassword = "23423"
+accountCity = "Delhi"
+// console.log(accountId);
+// console.log(accountId);
+// console.log(accountId);
+// console.log(accountId);
+console.table([accountId,accountEmail, accountPassword,accountCity,accountState])
