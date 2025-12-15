612ce2b760dab19390c4a92646db1a7f97293bd6
Mohd. Umer Ansari <ua7088807748@gmail.com>
2025-12-15 17:20:33 +0000

    Data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // We are using node js not browser
+console.log(3+3) 
+console.log("Umer"); // Code readability should be high
+// Original documentation of js in this website: tc39.es[ECMAScript] and MDN documentation
+
+let name= "Umer"
+let age= 20
+let isLogged= false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => to find uniqueness
+
+
+
+// object
+console.log(typeof "Umer");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
