b0349b740d2629f438a8bcb6defe3b23c82c3292
Mohd. Umer Ansari <ua7088807748@gmail.com>
2025-12-15 17:31:36 +0000

    Conversion of data types

---
+++ 01_basic/03_dataTypeConversion.js
@@ -0,0 +1,23 @@
+// let score = "Umer"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+//  "33" => 33
+//  "33abc" => NaN
+//   true => 1; false => 0
+
+let isLoggedIn = "Umer"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "Umer" => true
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
