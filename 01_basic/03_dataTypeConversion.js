57bf7a0849e5ef24cccd40324bb144074f821faf
Mohd. Umer Ansari <ua7088807748@gmail.com>
2025-12-16 17:26:22 +0000

    Operations

--- 01_basic/03_dataTypeConversion.js
+++ 01_basic/03_dataTypeConversion.js
@@ -10,14 +10,57 @@
 //  "33abc" => NaN
 //   true => 1; false => 0
 
-let isLoggedIn = "Umer"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "Umer"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// "" => false
-// "Umer" => true
-let someNumber =33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// // "" => false
+// // "Umer" => true
+// let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+// ********************** Operations ************************
+
+let value = 3 
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="Umer"
+let str2=" Ansari"
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+"2"+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(true+); // show an error
+// console.log(+false);
+// console.log(+"");
+// console.log(+"true");
+let num1, num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter++);
+// console.log(++gameCounter);
+
+
+
+
 
 
